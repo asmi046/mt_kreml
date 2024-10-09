2d1ec8a29beaaf4c8a9e50ead8f01d908a5b06ef
asmi046 <asmi046@gmail.com>
2024-10-09 18:09:25 +0300

    Изменения

--- resources/views/components/menu-puncts.blade.php
+++ resources/views/components/menu-puncts.blade.php
@@ -1,6 +1,6 @@
 <ul class="menue_puncts_list">
-    <li><a href="#about">О предложении</a></li>
-    <li><a href="#location">Топ 10 локаций</a></li>
+    <li><a href="#about">О туре</a></li>
+    <li><a href="#location">Лучшие моменты представления</a></li>
     <li><a href="#program">Программа тура</a></li>
     <li><a href="#contacts">Контакты</a></li>
 </ul>
